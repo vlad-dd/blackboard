0272afd0efcff2809a889572693da9e5ff99bb2c
ヴラド <55253594+vlad-dd@users.noreply.github.com>
2023-08-23 06:31:27 +0300

    Update index.ts

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { buildHeadersTokenObject } from "./utils";
 
 export const instanse = axios.create({
-    baseURL: process.env.BASE_API_URL
+    baseURL: process.env.REACT_APP_BASE_APU_URL
 });
 
 export const API = {
